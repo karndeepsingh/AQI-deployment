2f7c32e0fce931b065119a966f528c0dec284af2
Karndeep Singh <49562460+karndeepsingh@users.noreply.github.com>
2020-08-12 14:27:00 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,23 +2,43 @@ from flask import Flask,render_template,url_for,request
 import pandas as pd 
 
 import pickle
+import streamlit as st
 
+# classifier=pickle.load(pickle_in)
 # load the model from disk
-loaded_model=pickle.load(open('random_forest_regression_model.pkl', 'rb'))
-app = Flask(__name__)
+loaded_model=pickle.load(open('random_forest_new-Copy1.pkl', 'rb'))
+# app = Flask(__name__)
 
-@app.route('/')
-def home():
-	return render_template('home.html')
+# # @app.route('/')
+# def home():
+# 	return render_template('home.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    df=pd.read_csv('real_2018.csv')
-    my_prediction=loaded_model.predict(df.iloc[:,:-1].values)
-    my_prediction=my_prediction.tolist()
-    return render_template('result.html',prediction = my_prediction)
+# @app.route('/predict',methods=['POST'])
+def predict(Average_Temp,Max_Temp,Min_Temp,Atm_Pressure,Average_visibility):
+    prediction = loaded_model.predict([[Average_Temp,Max_Temp,Min_Temp,Atm_Pressure,Average_visibility]])
+    return prediction
 
 
+def main():
+    st.title("DELHI AIR QUALITY PREDICTION")
+    html_temp = """
+    <div style="background-color:tomato;padding:10px">
+    <h2 style="color:white;text-align:center;">DELHI AIR QUALITY PREDICTION APP </h2>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    Average_Temp = st.text_input("Average_Temp","Type Here")
+    Max_Temp= st.text_input("Max_Temp","Type Here")
+    Min_Temp = st.text_input("Min_Temp","Type Here")
+    Atm_Pressure = st.text_input("Atm_Pressure","Type Here")
+    Average_visibility = st.text_input("Average_visibility","Type Here") 
+    result=""
+    if st.button("Predict the Air Quality"):
+        result=predict(Average_Temp,Max_Temp,Min_Temp,Atm_Pressure,Average_visibility)
+    st.success('Predicted PM2.5 for today is {}'.format(result))
+    if st.button("About"):
+        st.text("Developed By KARNDEEP SINGH")
+        st.text("Manipal Academy Of Higher Education")
 
-if __name__ == '__main__':
-	app.run(debug=True)
+if __name__=='__main__':
+    main()
